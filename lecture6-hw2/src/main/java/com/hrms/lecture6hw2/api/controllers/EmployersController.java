e7b7e08556d827d85cb01a2cae53139dceb79f9c
CosmicDust19 <smh01.2019@gmail.com>
2021-05-11 14:00:54 +0300

    lecture6-hw2

--- lecture6-hw2/src/main/java/com/hrms/lecture6hw2/api/controllers/EmployersController.java
+++ lecture6-hw2/src/main/java/com/hrms/lecture6hw2/api/controllers/EmployersController.java
@@ -1,4 +1,27 @@
 package com.hrms.lecture6hw2.api.controllers;
 
+import com.hrms.lecture6hw2.business.abstracts.EmployerService;
+import com.hrms.lecture6hw2.entities.concretes.Employer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employers")
 public class EmployersController {
+
+    private final EmployerService employerService;
+
+    @Autowired
+    public EmployersController(EmployerService employerService) {
+        this.employerService = employerService;
+    }
+
+    @GetMapping("/getall")
+    public List<Employer> getAll(){
+        return employerService.getAll();
+    }
 }
--- lecture6-hw2/src/main/java/com/hrms/lecture6hw2/api/controllers/JobsController.java
+++ lecture6-hw2/src/main/java/com/hrms/lecture6hw2/api/controllers/JobsController.java
@@ -1,4 +1,27 @@
 package com.hrms.lecture6hw2.api.controllers;
 
+import com.hrms.lecture6hw2.business.abstracts.JobService;
+import com.hrms.lecture6hw2.entities.concretes.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobs")
 public class JobsController {
+
+    private final JobService jobService;
+
+    @Autowired
+    public JobsController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/getall")
+    public List<Job> getAll(){
+        return jobService.getAll();
+    }
 }
