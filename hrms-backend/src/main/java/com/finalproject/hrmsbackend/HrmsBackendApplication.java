524663332accc9d1a3eace53a65a940bb3e72293
CosmicDust19 <smh01.2019@gmail.com>
2021-06-21 13:21:11 +0300

    v12.2

--- hrms-backend/src/main/java/com/finalproject/hrmsbackend/HrmsBackendApplication.java
+++ hrms-backend/src/main/java/com/finalproject/hrmsbackend/HrmsBackendApplication.java
@@ -2,6 +2,7 @@ package com.finalproject.hrmsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableJpaAuditing
-public class HrmsBackendApplication {
+public class HrmsBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HrmsBackendApplication.class, args);
